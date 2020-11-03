ecb15a17b20a8b1dc4f4e9114c915ba6dce90fb1
DogaruAlexandru <dogaru.d.alexandru@gmail.com>
2020-11-03 10:30:05 +0200

    Panels format

--- Checkers Game/src/TwoPlayersPanel.java
+++ Checkers Game/src/TwoPlayersPanel.java
@@ -1,68 +1,55 @@
-import java.awt.*;
+import java.awt.Font;
+import java.awt.Graphics;
 
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class TwoPlayersPanel extends JPanel {
-    JLabel title;
-    JLabel Player1;
-    JLabel Player2;
-    JLabel Score;
-    JButton Reset;
-    JButton Back;
+    private final MyBoard myBoard;
+    private final JLabel player1;
+    private final JLabel player2;
+    private final JLabel score;
+    private final JButton reset;
+    private final JButton back;
+
     public TwoPlayersPanel() {
-        MyBoard myBoard = new MyBoard();
-        title = new JLabel("Checkers");
-        title.setBounds(385, 5, 250, 50);
-        title.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 60));
-        title.setForeground(Color.BLACK);
-        //title.setBackground(Color.BLACK);
-        title.setOpaque(true);
-        Player1 = new JLabel("Player1");
-        Player1.setBounds(730, 150, 180, 50);
-        Player1.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
-        Player1.setForeground(Color.BLACK);
-        //Player1.setBackground(Color.BLACK);
-        Player1.setOpaque(true);
-        Player1.setHorizontalTextPosition(SwingConstants.CENTER);
-        // Player1.set
-        Score= new JLabel("0-0");
-        Score.setBounds(770, 230, 55, 50);
-        Score.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
-        Score.setForeground(Color.BLACK);
-        //Score.setBackground(Color.BLACK);
-        Score.setOpaque(true);
-        Player2= new JLabel("Player2");
-        Player2.setBounds(730, 300, 180, 50);
-        Player2.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
-        Player2.setForeground(Color.BLACK);
-        //Computer.setBackground(Color.BLACK);
-        Player2.setOpaque(true);
-        Reset=new JButton("Reset");
-        Reset.setBounds(730,510,150,50);
-        Reset.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
-        Reset.setForeground(Color.BLACK);
-        //Reset.setBackground(Color.BLACK);
-        Reset.setOpaque(true);
-        Back=new JButton("Back");
-        Back.setBounds(730,460,150,50);
-        Back.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
-        Back.setForeground(Color.BLACK);
-        //Back.setBackground(Color.BLACK);
-        Back.setOpaque(true);
-        this.add(title);
-        this.add(Player1);
-        this.add(Player2);
-        this.add(Score);
-        this.add(Reset);
-        this.add(Back);
-        this.setLayout(null);
+        setLayout(null);
+
+        myBoard = new MyBoard();
+        player1 = new JLabel("Player1");
+        score = new JLabel("0-0");
+        player2 = new JLabel("Player2");
+        reset = new JButton("Reset");
+        back = new JButton("Back");
+
+        player1.setBounds(730, 150, 180, 50);
+        score.setBounds(765, 200, 55, 50);
+        player2.setBounds(728, 242, 180, 50);
+        reset.setBounds(715, 470, 175, 30);
+        back.setBounds(715, 505, 175, 30);
+
+        player1.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
+        score.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
+        player2.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 40));
+        reset.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 20));
+        back.setFont(new Font("TIMES NEW ROMAN", Font.BOLD, 20));
+
+        add(player1);
+        add(score);
+        add(player2);
+        add(reset);
+        add(back);
+
+        back.addActionListener(e -> Choose.OpenMenu());
+        reset.addActionListener(e -> Choose.OpenOnePlayerPanel());
     }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawRect(70,60,620,620);
-        g.drawRect(100,90,560,560);
-        for(int index=100;index<=630;index+=70)
-            for(int index1=90;index1<=630;index1+=70)
-                g.drawRect(index,index1,70,70);
+        g.drawRect(50, 50, 560, 560);
+        for (int index1 = 70; index1 < 590; index1 += 65)
+            for (int index2 = 70; index2 < 590; index2 += 65)
+                g.drawRect(index1, index2, 65, 65);
     }
 }
